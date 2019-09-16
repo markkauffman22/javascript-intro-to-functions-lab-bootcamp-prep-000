8803867e6f062d0fef3f2ed44814bdeadb4043f5
Mark Kauffman <markkauffman22@gmail.com>
2019-09-16 13:43:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function logShout(string){
   return console.log(string.toUpperCase())
 }
 
-function logShout(string){
-  return console.log(string.toUpperCase())
+function logWhisper(string){
+  return console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
